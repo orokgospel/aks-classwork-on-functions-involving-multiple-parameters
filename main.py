b0dd1e25ca67e887415d80d0eec06251b1cc565f
Gospel Theodore Orok <orokgospel@gmail.com>
2020-05-17 09:22:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+def print_full_name(first_name, middle_name, last_name):
+    print("your full name is:")
+    print(first_name)
+    print(middle_name)
+    print(last_name)
+print("I will call the function now")
+print_full_name("Gospel","Theodore", "Orok")
+
+
+def print_full_name(first_name, middle_name, last_name):
+    print("your full name is:")
+    print(first_name)
+    print(middle_name)
+    print(last_name)
+print("I will call the function now")
+print_full_name("Gospel","Theodore", "Orok")
+
+#EXERCISE
+def library_books(Book1, Book2, Book3, Book4):
+    print("choose any book")
+    print(Book1)
+    print(Book2)
+    print(Book3)
+    print(Book4)
+print("what class of book do you want?")
+library_books("Book1","Book2", "Book3", "Book4")
+
+
+def profile_details(name,age,gender,state,skill):
+    print("Submit")
+    print("State any of your profile details:")
+    print(name,age,gender,state,skill)
+    return 
+def progress(level,score):
+    print("what are your progress details?")
+    print(level,score)
+    return
+
+progress("level1", 90) 
+profile_details("Orok", "80","M", "AKS","Engr")
+
+
+
+
+
+
